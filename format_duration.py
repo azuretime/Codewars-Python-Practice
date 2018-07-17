d9dd66d051a16a6836fe7dec9cb44fdde083b449
azuretime <32590815+azuretime@users.noreply.github.com>
2018-07-17 17:24:03 +0800

    Create format_duration.py

---
+++ format_duration.py
@@ -0,0 +1,45 @@
+#Human readable duration format
+def format_duration(seconds):
+    if seconds == 0 : return 'now'
+    intervals = (
+        ('years', 31536000),  # 60 * 60 * 24 * 365
+        ('days', 86400),    # 60 * 60 * 24
+        ('hours', 3600),    # 60 * 60
+        ('minutes', 60),
+        ('seconds', 1),
+        )
+
+    result = []
+
+    for name, count in intervals:
+        value = seconds // count
+        if value:
+            seconds -= value * count
+            if value == 1:
+                name = name.rstrip('s')
+            result.append("{} {}".format(value, name))
+    return ', '.join(result[:-1])+' and '+result[-1] if len(result)>1 else result[0]
+
+
+times = [("year", 365 * 24 * 60 * 60), 
+         ("day", 24 * 60 * 60),
+         ("hour", 60 * 60),
+         ("minute", 60),
+         ("second", 1)]
+
+def format_duration(seconds):
+
+    if not seconds:
+        return "now"
+
+    chunks = []
+    for name, secs in times:
+        qty = seconds // secs
+        if qty:
+            if qty > 1:
+                name += "s"
+            chunks.append(str(qty) + " " + name)
+
+        seconds = seconds % secs
+
+    return ', '.join(chunks[:-1]) + ' and ' + chunks[-1] if len(chunks) > 1 else chunks[0]
