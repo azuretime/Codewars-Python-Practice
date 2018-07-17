4c7eb18f4eedbd16fe8a07ff7f0d8912f046fdf9
azuretime <32590815+azuretime@users.noreply.github.com>
2018-07-17 15:07:10 +0800

    Create Valid Braces.py

---
+++ Valid Braces.py
@@ -0,0 +1,17 @@
+def validBraces(string):
+    braces = {"(": ")", "[": "]", "{": "}"}
+    stack = []
+    for character in string:
+        if character in braces.keys():
+            stack.append(character)
+        else:
+            if len(stack) == 0 or braces[stack.pop()] != character:
+                return False
+    return len(stack) == 0 
+    
+def validBraces(s):
+  while '{}' in s or '()' in s or '[]' in s:
+      s=s.replace('{}','')
+      s=s.replace('[]','')
+      s=s.replace('()','')
+  return s==''
