0b34556e63203efc63246e218804ad699527361f
azuretime <32590815+azuretime@users.noreply.github.com>
2018-07-17 16:35:03 +0800

    Create sum-of-pairs.py

---
+++ sum-of-pairs.py
@@ -0,0 +1,8 @@
+#https://www.codewars.com/kata/sum-of-pairs/python
+
+def sum_pairs(lst, s):
+    cache = set()
+    for i in lst:
+        if s - i in cache:
+            return [s - i, i]
+        cache.add(i)
